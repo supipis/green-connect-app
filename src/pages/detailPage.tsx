5fd5c74be650395516be0e0dc25745d812ab5019
supipis <nirmanialgama@gmail.com>
2024-07-15 21:06:37 +0200

    implement the form to add new listing

---
+++ src/pages/detailPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {}
+
+const DetailPage = (props: Props) => {
+    const {} = props
+
+    return (
+        <div>hello</div>
+    )
+}
+
+export default DetailPage
