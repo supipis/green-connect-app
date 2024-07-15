43dbb34fe275ebd40bd1f4922521914e1ad7af15
supipis <nirmanialgama@gmail.com>
2024-07-15 15:00:13 +0200

    add the componenet listing card

---
+++ src/componenets/listingCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface Props {}
+
+function ListingCard(props: Props) {
+    const {} = props
+
+    return (
+        <>
+        <div>
+            
+        </div>
+        </>
+    )
+}
+
+export default ListingCard
