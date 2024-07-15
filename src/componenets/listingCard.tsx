47379f720c534af2b0f1166018ebce9a04c97b71
supipis <nirmanialgama@gmail.com>
2024-07-15 15:05:59 +0200

    set up the listing card content

--- src/componenets/listingCard.tsx
+++ src/componenets/listingCard.tsx
@@ -8,7 +8,23 @@ function ListingCard(props: Props) {
     return (
         <>
         <div>
-            
+            <img src="src/assets/tomato.png" alt="" />
+            <h1>Tomato</h1>
+            <div>
+            <div>
+                <p>Catagory</p>
+                <h3>Plant</h3>
+            </div>
+            <div>
+                <p>Location</p>
+                <h3>Solna</h3>
+            </div>
+            </div>
+            <div>
+            <p>Qty</p>
+            <h3>3</h3>
+            </div>
+
         </div>
         </>
     )
