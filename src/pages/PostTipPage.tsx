cc91a57dbd72c611bb3444e3fd0ad5201118ab69
supipis <nirmanialgama@gmail.com>
2024-07-15 22:18:59 +0200

    implement post tip page

---
+++ src/pages/PostTipPage.tsx
@@ -0,0 +1,11 @@
+const PostTipPage = () => {
+    return (
+        <div className="h-full w-full bg-custom-primary">
+        <div className="px-6 py-4">
+            <h1 className="text-custom-font-primary font-inika mb-6 text-lg ">Post a tip</h1>
+            </div>
+            </div>
+    )
+}
+
+export default PostTipPage
