fed810536a3d5cd974c3b209001bb1f0fefbd543
supipis <nirmanialgama@gmail.com>
2024-07-16 10:29:51 +0200

    styling adjustments

--- src/componenets/listingCard.tsx
+++ src/componenets/listingCard.tsx
@@ -18,26 +18,20 @@ const ListingCard = ({ id, image, name, category, location, quantity }: Props) =
     
     return (
         <>
-        <div className='bg-white shadow-md rounded-lg flex items-center p-4 mb-4'
+        <div className='bg-white shadow-md rounded-lg flex items-center p-4 mb-4 cursor-pointer gap-10'
         onClick={handleCardClick}
         >
-            <img src={image} alt="" className='w-16 h-16 rounded mr-6' />
-            <div className='flex-1'>
-            <h1 className='text-lg text-custom-font-primary font-inika font-bold'>{name}</h1>
-            <div className="text-sm">
-            <div className='flex'>
-                <p className="text-custom-font-primary font-inika mr-1">Catagory: </p>
-                <p className='text-custom-font-primary font-inika font-semibold'>{category}</p>
+            <div>
+            <img src={image} alt="name" className='w-16 h-16 rounded ' />
             </div>
-            <div className='flex'>
-                <p className='text-custom-font-primary font-inika mr-1'>Location</p>
-                <p className='text-custom-font-primary font-inika font-semibold'>{location}</p>
+            
+            <div className=''>
+            <h1 className='text-lg text-custom-font-primary font-inika font-bold mb-2'>{name}</h1>
+            <div className="text-sm mb-2">
+                <p className='text-custom-font-primary font-inika'>Category: <span className='font-semibold'>{category}</span></p>
+                <p className='text-custom-font-primary font-inika'>Location: <span className='font-semibold'>{location}</span></p>
+                <p className="text-custom-font-primary font-inika">Qty: <span className='font-semibold'>{quantity}</span></p>
             </div>
-            </div>
-            </div>
-            <div className="ml-4 text-center text-sm">
-            <p className="text-sm  text-custom-font-primary font-inika">Qty</p>
-            <p className="text-lg font-semibold text-custom-font-primary font-inika">{quantity}</p>
             </div>
         </div>
         </>
