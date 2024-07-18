68e8992452090079daf6a1ee4c596d7d2c78bee0
supipis <nirmanialgama@gmail.com>
2024-07-18 15:56:59 +0200

    remove unused files

---
+++ src/componenets/PostCards.tsx
@@ -0,0 +1,25 @@
+// type Props = {
+//     tip: string;
+//     postedBy: string;
+//     postedOn: string
+// }
+
+// const TipCards = ({tip, postedBy, postedOn}: Props) => {
+    
+
+//     return (
+//         <div className='bg-white shadow-md rounded-lg  p-4 mb-4'>
+//             <h2 className='text-custom-font-primary font-inika font-semibold text-lg'>{tip}</h2>
+//             <div className='flex justify-between'>
+//             <p className='text-custom-font-primary font-inika text-sm'>Posted by: {postedBy}</p>
+//             <p className='text-custom-font-primary font-inika text-sm'>on: {postedOn}</p>
+//             </div>
+//            <div>
+          
+//            </div>
+            
+//             </div>
+//     )
+// }
+
+// export default TipCards
---
+++ src/pages/PostPage.tsx
@@ -0,0 +1,31 @@
+// import { useNavigate } from "react-router-dom";
+
+
+
+
+// const Tips = () => {
+//   const navigate = useNavigate();
+
+  
+
+//   return (
+//     <div className="h-full w-full lg:mt-20 md:mt-20">
+//       <div className="px-6 py-4 lg:px-12 lg:py-8">
+//         <h1 className="text-custom-font-primary font-inika mb-6 text-lg ">
+//           Let's grow together
+//         </h1>
+       
+//       </div>
+//       <div className="text-center">
+//         <button
+//           className="bg-custom-btn-primary font-inika text-custom-btn-txt py-3 px-12 rounded-lg mt-2 font-bold text-xl"
+//           onClick={() => navigate("/post")}
+//         >
+//           Post
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Tips;
