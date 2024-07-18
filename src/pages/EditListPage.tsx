c5cb7776635ee92c2838bdc83ac3e403a42fc151
supipis <nirmanialgama@gmail.com>
2024-07-18 09:40:41 +0200

    remove the edit button and moved to the edit componenet

--- src/pages/EditListPage.tsx
+++ src/pages/EditListPage.tsx
@@ -19,11 +19,6 @@ const EditListPage = () => {
         </div>
         <EditList />
       </div>
-      <div className="text-center">
-        <button className="bg-custom-btn-primary text-custom-btn-txt py-3 px-12 rounded-lg mt-2 font-bold text-xl font-inika">
-          Edit
-        </button>
-      </div>
     </div>
   );
 };
