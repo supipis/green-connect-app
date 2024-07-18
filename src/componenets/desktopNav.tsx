7be3f9881462aa1e5b9e18017748c1061e2bfa23
supipis <nirmanialgama@gmail.com>
2024-07-18 09:38:30 +0200

    change the format of the logo to avoid compile issues

--- src/componenets/desktopNav.tsx
+++ src/componenets/desktopNav.tsx
@@ -1,13 +1,13 @@
 import { NavLink } from "react-router-dom";
 import { Icon } from '@iconify/react/dist/iconify.js';
-
+import logoImageUrl from "../assets/logo.png"
 const DesktopNav = () => {
   return (
     <div className="bg-white shadow-lg fixed top-0 left-0 w-full z-10">
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
         {/* Logo */}
         <div className="text-custom-font-primary font-bold text-2xl">
-          <img src="src/assets/logo.png" alt="" className="h-10"/>
+          <img src={logoImageUrl} alt="" className="h-10"/>
         </div>
         
         {/* Navigation Links */}
@@ -53,7 +53,7 @@ const DesktopNav = () => {
         <NavLink
             to="/login"
             className="flex items-center text-custom-font-primary  hover:text-custom-btn-primary"
-            activeClassName="text-custom-btn-primary"
+            
           >
             <Icon icon="mdi:login" className="w-6 h-6 mr-1" />
             Login
