33503a9872c12f47c6aeed03bfd0fecec4860134
supipis <nirmanialgama@gmail.com>
2024-07-19 02:03:08 +0200

    styling

--- src/pages/EditListPage.tsx
+++ src/pages/EditListPage.tsx
@@ -1,25 +1,34 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
-import EditList from "../componenets/editList";
 import { useNavigate } from "react-router-dom";
+import EditList from '../componenets/editList';
+
 
 const EditListPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  
   return (
-    <div className="h-full w-full lg:mt-20 md:mt-20">
-      <div className="lg:px-12 lg:py-8 px-6 py-4">
-      <div className="flex">
-        <button 
-          className="mr-6"
-          onClick={() => navigate(-1)}
-        ><Icon icon="weui:back-outlined" />
-        </button>
-        <h1 className="text-custom-font-primary font-inika text-lg ">
-          My listings
-        </h1>
+    <>
+      <div className="lg:mt-20 md:mt-20">
+        <div className="lg:px-12 lg:py-8 px-6 py-4">
+          <div className="w-full flex items-center mb-6">
+            <button 
+              className="mr-6"
+              onClick={() => navigate(-1)}
+            >
+              <Icon icon="weui:back-outlined" />
+            </button>
+            <h1 className="text-custom-font-primary font-inika text-lg">
+              My listings
+            </h1>
+          </div>
+          <div className="flex flex-col items-center justify-center ">
+            <div className="w-full max-w-3xl bg-white p-10 shadow-lg rounded-lg">
+              <EditList />
+            </div>
+          </div>
         </div>
-        <EditList />
       </div>
-    </div>
+    </>
   );
 };
 
